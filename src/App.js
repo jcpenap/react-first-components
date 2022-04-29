4ccf3d8b852a940c105ea1834221ad27f0d7fd18
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2022-04-29 09:57:32 -0500

    Adding normal JavaScript Logic to components

--- src/App.js
+++ src/App.js
@@ -7,25 +7,25 @@ function App() {
       id: "e1",
       title: "Toiled Paper",
       amount: 94.12,
-      date: new Date(2020, 7, 14),
+      date: new Date(2020, 7, 12),
     },
     {
       id: "e2",
       title: "New TV",
       amount: 294.67,
-      date: new Date(2020, 7, 14),
+      date: new Date(2020, 3, 16),
     },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 458.12,
-      date: new Date(2021, 7, 14),
+      date: new Date(2021, 10, 14),
     },
     {
       id: "e4",
       title: "Toiled Paper",
       amount: 94.12,
-      date: new Date(2020, 7, 14),
+      date: new Date(2020, 2, 5),
     },
   ];
 
