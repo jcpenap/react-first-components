1f97051c1591c53d18e6534e2b6a2d55387640b4
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2022-05-06 18:15:42 -0500

    Adding two way binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,48 +6,37 @@ const ExpenseForm = () => {
   const [enteredAmout, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
 
-    // const [userInput, setUserInput] = useState({
-    //     enteredTitle: "",
-    //     enteredAmout: "",
-    //     enteredDate: ""
-    // });
-
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
-    // setUserInput({
-    //     ...userInput,
-    //     enteredTitle: event.target.value
-    // })
-    // setUserInput((prevState) => {
-    //   return {
-    //     ...prevState, 
-    //     enteredTitle: event.target.value
-    //   }
-    // });
   };
 
   const amoutChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
-    // setUserInput({
-    //     ...userInput,
-    //     enteredAmout: event.target.value
-    // })
+    setEnteredAmount(event.target.value)
   };
 
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
-    // setUserInput({
-    //     ...userInput,
-    //     enteredDate: event.target.value
-    // })
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amout: enteredAmout,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData)
+    setEnteredTitle('')
+    setEnteredAmount('')
+    setEnteredDate('')
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amout</label>
@@ -55,6 +44,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmout}
             onChange={amoutChangeHandler}
           />
         </div>
@@ -64,6 +54,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
